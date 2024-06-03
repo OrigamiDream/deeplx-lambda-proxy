7415c51b38db385a83ee465627de0b3adba57413
OrigamiDream <hello@origamidream.me>
2024-06-03 16:16:25 +0900

    Update terraform outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,9 @@
 output "alb_dns_name" {
   value = module.alb.dns_name
 }
+
+output "proxy_commit_urls" {
+  value = [
+    for i in range(var.lambda_size): "http://${module.alb.dns_name}/v${i}/commit"
+  ]
+}
